391bc4f72eb519e72508a1053fab6eaa61cccb60
millystuart <melissaaa.stuart@gmail.com>
2025-08-28 18:32:19 +0100

    Amend colours from RGB tuples to hex strings

--- tetrimino.js
+++ tetrimino.js
@@ -1,11 +1,13 @@
-// RGB colour values for each tetrimino
-const TURQUOISE = (70,255,255);
-const BLUE      = (0,60,255);
-const ORANGE    = (255,200,0);
-const YELLOW    = (255,250,0);
-const GREEN     = (0,250,0);
-const PURPLE    = (185,0,255);
-const RED       = (255,0,0);
+import {GRID_ROWS, GRID_COLS, gridBlocks} from "./grid.js";
+
+// Hex colour values for each tetrimino
+const TURQUOISE = "#40e0d0ff";
+const BLUE      = "#0000ff";
+const ORANGE    = "#ffa500";
+const YELLOW    = "#ffff00";
+const GREEN     = "#00ff00";
+const PURPLE    = "#800080";
+const RED       = "#ff0000";
 
 // Below is an exported object containing each tetrimino's four rotation states and correpsonding colour.
 // Each rotation state is represented by a 2D array, where 1 indicates the presence of a block.
