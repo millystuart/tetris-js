de2b1983c1d314780a237258fcea57d0796c5cfc
millystuart <melissaaa.stuart@gmail.com>
2025-08-28 18:04:12 +0100

    Generate 2D-array containing the blocks that make up the grid

--- grid.js
+++ grid.js
@@ -1,8 +1,16 @@
+export let gridBlocks = []; // Array holding each block in the grid
+
 // For number of rows and columns specified, appends that many blocks to the grid element
 export function initialiseGrid(rows, cols, grid) {
-    for (let i = 0; i < rows * cols; i++) {
-        let block = document.createElement("div");
-        block.classList.add("block");
-        grid.appendChild(block);
+    for (let row = 0; row < rows; row++) {
+        let rowBlocks = []; // Array to hold each block in the current row
+
+        for (let col = 0; col < cols; col++) {
+            let block = document.createElement("div");
+            block.classList.add("block");
+            grid.appendChild(block);
+            rowBlocks.push(block);
+        }
+        rowBlocks.push(gridBlocks); // Add the final row to the gridBlocks array
     }
 }
