1366bd519b5eebc6d474a9a47510cce7c27f2d5d
millystuart <melissaaa.stuart@gmail.com>
2025-08-28 18:12:24 +0100

    Remove redundant comment

--- grid.js
+++ grid.js
@@ -1,4 +1,4 @@
-export let gridBlocks = []; // Array holding each block in the grid
+export let gridBlocks = []; // Array to hold each block in the grid
 
 // For number of rows and columns specified, appends that many blocks to the grid element
 export function initialiseGrid(rows, cols, grid) {
@@ -11,6 +11,6 @@ export function initialiseGrid(rows, cols, grid) {
             grid.appendChild(block);
             rowBlocks.push(block);
         }
-        rowBlocks.push(gridBlocks); // Add the final row to the gridBlocks array
+        rowBlocks.push(gridBlocks);
     }
 }
