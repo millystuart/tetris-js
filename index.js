16d399fbec3cad22ed1beb3ec3f02cfbca83b884
millystuart <melissaaa.stuart@gmail.com>
2025-08-29 10:49:27 +0100

    Add additional comment to file for clarity

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ const GRID = document.getElementById("grid");
 // gridBlocks will hold the state of the grid
 let gridBlocks = initialiseGrid(GRID_ROWS, GRID_COLS, GRID);
 // Returns an array holding the shape and colour of a random tetrimino
-let tetrimino = generateRandomTetrimino();
+let tetrimino = generateRandomTetrimino(); // [0] is the shape, [1] is the colour
 
 drawTetrimino(tetrimino[0], tetrimino[1], 0, 4, gridBlocks);
