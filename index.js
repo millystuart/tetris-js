f40b74bbd183ba4825f2e79612471ed24f9df9e9
millystuart <melissaaa.stuart@gmail.com>
2025-08-29 14:47:53 +0100

    Successfully display a random tetrimino falling from the top of the grid

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-import {initialiseGrid, GRID_COLS, GRID_ROWS} from "./grid.js";
+import {initialiseGrid, clearGrid, GRID_COLS, GRID_ROWS} from "./grid.js";
 import {drawTetrimino, generateRandomTetrimino} from "./tetrimino.js";
 
 const GRID = document.getElementById("grid");
+let startRow = 0;
+let startCol = 3;
 
 // gridBlocks will hold the state of the grid
 let gridBlocks = initialiseGrid(GRID_ROWS, GRID_COLS, GRID);
-// Returns an array holding the shape and colour of a random tetrimino
-let tetrimino = generateRandomTetrimino(); // [0] is the shape, [1] is the colour
+let tetrimino = generateRandomTetrimino();
 
-drawTetrimino(tetrimino[0], tetrimino[1], 0, 4, gridBlocks);
+setInterval(gameLoop, 500);
+
+function gameLoop() {
+    descendTetrimino(tetrimino, gridBlocks)
+}
+
+function descendTetrimino(tetrimino, gridBlocks) {
+    // Start by clearing grid so that the tetrimino can be drawn in its new position without leaving a trail.
+    clearGrid(gridBlocks);
+    gridBlocks = drawTetrimino(tetrimino[0], tetrimino[1], startRow, startCol, gridBlocks);
+    startRow++;
+    console.log(startRow);
+
+    if (startRow > GRID_ROWS - tetrimino[0].length) {
+        startRow = 0; // Reset tetrimino to the top
+        console.log(gridBlocks)
+    }
+}
