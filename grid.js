2d449e6a9c2eaa0dce38a37ce95a2c221530df6c
millystuart <melissaaa.stuart@gmail.com>
2025-08-29 09:15:04 +0100

    Create utility function that 'clears' the grid

--- grid.js
+++ grid.js
@@ -1,5 +1,6 @@
 export const GRID_ROWS = 20;
 export const GRID_COLS = 10;
+const WHITE = "#FFFFFF";
 
 // For number of rows and columns specified, appends that many blocks to the grid element
 // This function returns the 2D array of blocks that make up the grid for external use
@@ -18,4 +19,14 @@ export function initialiseGrid(rows, cols, grid) {
         gridBlocks.push(rowBlocks);
     }
     return gridBlocks;
+}
+
+// This functions takes our 2D-array of blocks and resets them all to the default colour (white).
+// Note that clearGrid does not "clear" the grid by removing blocks, it simply resets their colour.
+export function clearGrid(gridBlocks) {
+    for (let row of gridBlocks) {
+        for (let col of row) {
+            drawBlock(row, col, WHITE, gridBlocks);
+        }
+    }
 }
