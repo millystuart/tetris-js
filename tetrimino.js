2e0cd697e155cc406c3b81a5407773f879019843
millystuart <melissaaa.stuart@gmail.com>
2025-08-29 11:21:25 +0100

    Successfully rework tetrimino file's functions to accept new block representation

--- tetrimino.js
+++ tetrimino.js
@@ -132,29 +132,40 @@ const TETRIMINOS = {
     },
 }
 
-function drawBlock(blockRow, blockCol, blockColour, gridBlocks) {
-    let block = gridBlocks[blockRow][blockCol];
-    console.log(block);
+function drawBlock(block, blockColour) {
     block.style.backgroundColor = blockColour;
 }
 
+function isValidBlockToOccupy(gridBlock, row, col) {
+    if ((row >= 0 && row < GRID_ROWS) && (col >= 0 && col < GRID_COLS)) { // check if the block is within the grid boundaries
+        if (!gridBlock.occupied) { // check if the block is already occupied
+            return true;
+        }
+    }
+    return false;
+}
+
 // Function to draw a tetrimino on the grid at a given position/rotation
+// drawTetrimino needs to return an updated gridBlocks array with the state of the board now that the tetrimino has been drawn
 export function drawTetrimino(tetriminoShape, colour, posRow, posCol, gridBlocks) {    
     for (let row = 0; row < tetriminoShape.length; row++) { // For each row in the tetrimino's shape
         for (let col = 0; col < tetriminoShape[row].length; col++) { // For each column in that row
             if (tetriminoShape[row][col] === 1) {
-                // Calculate the corresponding row/column in the grid based on desired position
-                const gridPosRow = posRow + row;
-                const gridPosCol = posCol + col;
+                // Calculate the required row/column in the grid for the block to be drawn based on specified position on the grid
+                const blockPosRow = posRow + row;
+                const blockPosCol = posCol + col;
+                const blockToDraw = gridBlocks[blockPosRow][blockPosCol];
                 
-                // Check if the calculated position is within the grid boundaries
-                if ((gridPosRow >= 0 && gridPosRow < GRID_ROWS) && (gridPosCol >= 0 && gridPosCol < GRID_COLS)) {
-                    drawBlock(gridPosRow, gridPosCol, colour, gridBlocks);
+                // Check if the calculated position is already occupied
+                if (isValidBlockToOccupy(blockToDraw, blockPosRow, blockPosCol)) {
+                    blockToDraw.occupied = true; // Mark the block as occupied
+                    drawBlock(blockToDraw.blockElement, colour, gridBlocks); // Draw the block on the grid
                 }
                 // TODO: Handle case where part of tetrimino is out of bounds (e.g., at top or sides of grid)
             }
         }
     }
+    return gridBlocks; // return the state of gridBlocks after drawing the tetrimino
 }
 
 // Function that will choose a random tetrimino of the seven available, and a given orientation between 0 and 3.
