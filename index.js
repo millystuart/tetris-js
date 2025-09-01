399c60e6e488f37cb9cce6817241d45725511c68
millystuart <melissaaa.stuart@gmail.com>
2025-09-01 15:10:08 +0100

    Amend game loop to make use of renderGrid function

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import {initialiseGrid, clearGrid, GRID_COLS, GRID_ROWS} from "./grid.js";
-import {drawTetrimino, generateRandomTetrimino} from "./tetrimino.js";
+import {initialiseGrid, clearGrid, GRID_COLS, GRID_ROWS, renderGrid} from "./grid.js";
+import {drawActiveTetrimino, generateRandomTetrimino} from "./tetrimino.js";
 
 const GRID = document.getElementById("grid");
 globalThis.gridBlocks = []; // GLOBAL array to hold each block on the grid
@@ -19,7 +19,9 @@ function gameLoop() {
 function descendTetrimino(tetrimino) {
     // Start by clearing grid so that the tetrimino can be drawn in its new position without leaving a trail.
     clearGrid(gridBlocks);
-    drawTetrimino(tetrimino[0], tetrimino[1], startRow, startCol, gridBlocks);
+    // Render all placed blocks onto the grid
+    renderGrid();
+    drawActiveTetrimino(tetrimino[0], tetrimino[1], startRow, startCol);
     startRow++;
 
     if (startRow > GRID_ROWS - tetrimino[0].length) {
