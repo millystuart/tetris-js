fe9902f3d5f3038e3405d3e834c97bc50e45bab7
millystuart <melissaaa.stuart@gmail.com>
2025-09-02 10:51:17 +0100

    Cause a tetrimino rotation on up arrow key press

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import {initialiseGrid, clearGrid, GRID_COLS, GRID_ROWS, renderGrid} from "./grid.js";
-import {drawTetrimino, generateRandomTetrimino, checkLeftCollision, checkRightCollision, checkVerticalCollision} from "./tetrimino.js";
+import {drawTetrimino, generateRandomTetrimino, checkLeftCollision, checkRightCollision, checkVerticalCollision, fetchNextRotation} from "./tetrimino.js";
 
 const GRID = document.getElementById("grid");
 globalThis.gridBlocks = []; // GLOBAL array to hold each block on the grid
@@ -56,6 +56,20 @@ function handleKeyInput(event) {
             // if there is no right collision, render the block in the new position!
             if (checkRightCollision(activeTetrimino[0], currentRow, currentCol) === false) {
                 currentCol++; // Increment column to reflect shape moving right by one block
+                clearGrid();
+                renderGrid();
+                drawTetrimino(activeTetrimino[0], activeTetrimino[1], currentRow, currentCol, false);
+            }
+            break;
+        
+        case "ArrowUp":
+            let proposedTetriminoRotation = fetchNextRotation(activeTetrimino[2], activeTetrimino[3]); // Passing in the string representing the type of tetrimino in play (e.g. "J")
+            // Check collisions to ensure that rotating won't move the piece to an invalid position
+            // No need to check vertical collisions as those are checked by the left/right collision functions
+            if (!checkLeftCollision(proposedTetriminoRotation, currentRow, currentCol) &&
+                !checkRightCollision(proposedTetriminoRotation, currentRow, currentCol)) {
+                activeTetrimino[0] = proposedTetriminoRotation; // Update the shape to the new proposed shape
+                activeTetrimino[3]++; // increment the rotation index to reflect the new tetrimino orientation
                 clearGrid();
                 renderGrid();
                 drawTetrimino(activeTetrimino[0], activeTetrimino[1], currentRow, currentCol, false);
